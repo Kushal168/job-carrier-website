13d0b3263da6f23da40363a9b1c6b461a11bea5d
Kushal Goyal <97747257+Kushal168@users.noreply.github.com>
2024-04-28 05:55:21 +0000

    Basic configuration for flask project

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print("helloooooooooooo")
+    return "Hello, World!!!!"
+
+if __name__ == '__main__':
+    print("inside if")
+    app.run(host='0.0.0.0', debug=True)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello_world():
+#   print("helloooooooooooo")
+
+
+# if (__name__ == '__main__'):
+#   print("insideeeennnnnnnnnifff")
+#   app.run(host='0.0.0.0',port=80, debug=True)
+  
+
+# # print("hello world!!")
+
+# # print(__name__)
